2a0d274f6194006e8094596f632b5a638a99b0d6
Kilian Kilger <kkilger@gmail.com>
2018-02-07 21:49:36 +0100

    Add drawing area; bump gtk

--- src/main.rs
+++ src/main.rs
@@ -33,26 +33,41 @@ fn run() -> Result<(), Error> {
 
 fn initialize_gtk() -> Result<(), Error> {
     gtk::init()?;
-    let _ = create_window()?;
+    let _ = initialize_display()?;
     gtk::main();
     Ok(()) 
 }
 
-fn create_window() -> Result<gtk::Window, Error> {
+fn initialize_display() -> Result<(), Error> {
+    if let Some(screen) = gdk::Screen::get_default() {
+        initialize_window(&screen)
+    } 
+    else {
+        Err(LabyrinthError::CouldNotGetDefaultScreen.into())
+    }
+}
+
+fn initialize_window(screen : & gdk::Screen) -> Result<(), Error> {
     use gtk::prelude::*;
+    use gdk::ScreenExt; 
+    let monitor = screen.get_primary_monitor();
     let window = gtk::Window::new(gtk::WindowType::Toplevel);
-    window.fullscreen();
+    window.fullscreen_on_monitor(screen, monitor);
     window.connect_delete_event(|_,_| {
         gtk::main_quit();
         gtk::Inhibit(true)
     });
-    window.connect_key_press_event(move |_, key| {
+    let drawing_area = gtk::DrawingArea::new();
+    let geometry = screen.get_monitor_geometry(monitor);
+    drawing_area.set_size_request(geometry.width, geometry.height);
+    window.add(&drawing_area);
+    window.connect_key_press_event(|_, key| {
         match key.get_keyval() {
             gdk::enums::key::Escape => gtk::main_quit(),
             _ => ()
         }
-        gtk::Inhibit(false)
+        gtk::Inhibit(true)
     });
     window.show_all(); 
-    return Ok(window);
+    Ok(())
 }
